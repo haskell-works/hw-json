e6d26c7a9d870b08a082b9899b26b6e8dfaa1ac4
John Ky <newhoggy@gmail.com>
2018-07-26 08:28:51 +1000

    New run commands and update to lts-12.2

--- run-stack.sh
+++ run-stack.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 
+STACK_FLAGS="--flag bits-extra:bmi2 --flag hw-rankselect-base:bmi2 --flag hw-rankselect:bmi2 --flag hw-json:bmi2"
+
 case $1 in
   build)
     stack build \
       --test --no-run-tests --bench --no-run-benchmarks \
-      --flag bits-extra:bmi2 --flag hw-rankselect-base:bmi2 --flag hw-rankselect:bmi2 --flag hw-json:bmi2
+      $STACK_FLAGS
     ;;
   test)
     stack test \
-      --flag bits-extra:bmi2 --flag hw-rankselect-base:bmi2 --flag hw-rankselect:bmi2 --flag hw-json:bmi2
+      $STACK_FLAGS
     ;;
 
   bench)
     stack bench \
-      --flag bits-extra:bmi2 --flag hw-rankselect-base:bmi2 --flag hw-rankselect:bmi2 --flag hw-json:bmi2
+      $STACK_FLAGS
+    ;;
+
+  install)
+    stack install \
+      $STACK_FLAGS
+    ;;
+
+  repl)
+    stack repl \
+      $STACK_FLAGS
+    ;;
+
+  bench)
+    stack bench \
+      $STACK_FLAGS
     ;;
 esac
