21ce1534fc01aaedd9aa457c3bbecafd4395c02d
George Wilson <george@wils.online>
2018-10-18 16:21:15 +1000

    Remove unused function and then resourcet dependency

--- app/App/IO.hs
+++ app/App/IO.hs
@@ -1,22 +1,7 @@
 module App.IO where
 
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Resource
-import System.IO                    (Handle)
-
 import qualified Data.ByteString.Lazy as LBS
 import qualified System.IO            as IO
-
-openOutputFile :: MonadResource m => FilePath -> Maybe Int -> m (ReleaseKey, Handle)
-openOutputFile "-" _ = allocate (return IO.stdout) (const (return ()))
-openOutputFile filePath maybeBufferSize = allocate open close
-  where open  = do
-          handle <- IO.openFile filePath IO.WriteMode
-          forM_ maybeBufferSize $ \bufferSize ->
-            liftIO $ IO.hSetBuffering handle (IO.BlockBuffering (Just bufferSize))
-          return handle
-        close = IO.hClose
 
 readInputFile :: FilePath -> IO LBS.ByteString
 readInputFile "-"      = LBS.hGetContents IO.stdin
