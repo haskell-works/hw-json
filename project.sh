15ebf3f15d4e2f47b94c9f60ee41019288cac845
John Ky <newhoggy@gmail.com>
2018-12-25 12:29:17 -0800

    New clean command for project.sh

--- project.sh
+++ project.sh
@@ -8,30 +8,39 @@ shift
 
 case "$cmd" in
   install)
-    cabal new-install \
+    cabal new-install -j4 \
       --test --no-run-tests --bench --no-run-benchmarks \
       $CABAL_FLAGS "$@"
     ;;
 
   build)
-    cabal new-build all \
+    cabal new-build all -j4 \
       --disable-tests --disable-benchmarks \
       $CABAL_FLAGS "$@"
     ;;
 
   test)
-    cabal new-test \
+    cabal new-test -j4 \
       $CABAL_FLAGS "$@"
     ;;
 
   bench)
-    cabal new-bench \
+    cabal new-bench -j4 \
       $CABAL_FLAGS "$@"
     ;;
 
   repl)
     cabal new-repl \
       $CABAL_FLAGS "$@"
+    ;;
+
+  clean)
+    cabal new-clean
+    ;;
+  
+  *)
+    echo "Unrecognised command: $cmd"
+    exit 1
     ;;
 esac
 
