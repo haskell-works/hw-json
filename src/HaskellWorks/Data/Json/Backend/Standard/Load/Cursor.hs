1159846415e2f6371248e9194f1a6ff8b7c772ca
John Ky <newhoggy@gmail.com>
2019-01-29 21:39:37 +1100

    Move relevant functions to Load.Cursor module

--- src/HaskellWorks/Data/Json/Backend/Standard/Load/Cursor.hs
+++ src/HaskellWorks/Data/Json/Backend/Standard/Load/Cursor.hs
@@ -7,8 +7,12 @@ module HaskellWorks.Data.Json.Backend.Standard.Load.Cursor
 import Data.Word
 import HaskellWorks.Data.BalancedParens.Simple
 import HaskellWorks.Data.Json.Backend.Standard.Cursor
+import HaskellWorks.Data.Json.Backend.Standard.Load.Raw
+import HaskellWorks.Data.RankSelect.CsPoppy
+import HaskellWorks.Data.RankSelect.Poppy512
 
 import qualified Data.ByteString                              as BS
+import qualified Data.ByteString.Internal                     as BSI
 import qualified Data.Vector.Storable                         as DVS
 import qualified HaskellWorks.Data.ByteString                 as BS
 import qualified HaskellWorks.Data.Json.Backend.Standard.Slow as SLOW
@@ -17,4 +21,29 @@ loadCursor :: String -> IO (JsonCursor BS.ByteString (DVS.Vector Word64) (Simple
 loadCursor path = do
   bs <- BS.mmap path
   let !cursor = SLOW.makeCursor bs
+  return cursor
+
+loadCursorWithIndex :: String -> IO (JsonCursor BSI.ByteString (DVS.Vector Word64) (SimpleBalancedParens (DVS.Vector Word64)))
+loadCursorWithIndex filename = do
+  (jsonBS, jsonIb, jsonBp) <- loadRawWithIndex filename
+  let cursor = JsonCursor jsonBS jsonIb (SimpleBalancedParens jsonBp) 1
+  return cursor
+
+loadCursorWithPoppy512Index :: String -> IO (JsonCursor BSI.ByteString Poppy512 (SimpleBalancedParens (DVS.Vector Word64)))
+loadCursorWithPoppy512Index filename = do
+  (jsonBS, jsonIb, jsonBp) <- loadRawWithIndex filename
+  let cursor = JsonCursor jsonBS (makePoppy512 jsonIb) (SimpleBalancedParens jsonBp) 1
+  return cursor
+
+loadCursorWithCsPoppyIndex :: String -> IO (JsonCursor BSI.ByteString CsPoppy (SimpleBalancedParens (DVS.Vector Word64)))
+loadCursorWithCsPoppyIndex filename = do
+  (jsonBS, jsonIb, jsonBp) <- loadRawWithIndex filename
+  let cursor = JsonCursor jsonBS (makeCsPoppy jsonIb) (SimpleBalancedParens jsonBp) 1
+  return cursor
+
+loadCursorWithPoppy512Index2 :: String -> IO (JsonCursor BSI.ByteString Poppy512 (SimpleBalancedParens Poppy512))
+loadCursorWithPoppy512Index2 filename = do
+  (jsonBS, jsonIb, jsonBp) <- loadRawWithIndex filename
+  let cursor = JsonCursor jsonBS (makePoppy512 jsonIb) (SimpleBalancedParens (makePoppy512 jsonBp)) 1
+                :: JsonCursor BSI.ByteString Poppy512 (SimpleBalancedParens Poppy512)
   return cursor
