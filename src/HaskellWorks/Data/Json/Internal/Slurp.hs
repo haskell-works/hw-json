0342a96431f12aa443716ffae601dcdf7e3cd447
John Ky <newhoggy@gmail.com>
2019-10-24 20:42:08 +1100

    Add doctest

--- src/HaskellWorks/Data/Json/Internal/Slurp.hs
+++ src/HaskellWorks/Data/Json/Internal/Slurp.hs
@@ -24,11 +24,29 @@ data JsonState
   | InNumber
   | InIdent
 
+-- | Slurp a JSON string
+--
+-- Examples:
+--
+-- >>> :set -XOverloadedStrings
+-- >>> slurpText "\"Hello\""
+-- Right "Hello"
+-- >>> slurpText "123"
+-- Left "34: Failed reading: satisfy"
 slurpText :: BS.ByteString -> Either Text Text
 slurpText bs = case PBS.parseOnly AP.jstring bs of
   Right t -> Right t
   Left e  -> Left (T.pack e)
 
+-- | Slurp a JSON number
+--
+-- Examples:
+--
+-- >>> :set -XOverloadedStrings
+-- >>> slurpNumber "123, true"
+-- "123"
+-- >>> slurpNumber "\"Hello\""
+-- "\"Hello\""
 slurpNumber :: BS.ByteString -> BS.ByteString
 slurpNumber bs = let (!cs, _) = BS.unfoldrN (BS.length bs) genNumber (InJson, bs) in cs
     where genNumber :: (JsonState, BS.ByteString) -> Maybe (Word8, (JsonState, BS.ByteString))
